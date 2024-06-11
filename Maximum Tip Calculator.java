5b8f526f2cceff6d76686366496d87ceaadc5ac9
Sharad Singh Kushwaha <153629616+absolutely-sharad@users.noreply.github.com>
2024-06-11 20:42:59 +0530

    Create Maximum Tip Calculator.java

---
+++ Maximum Tip Calculator.java
@@ -0,0 +1,47 @@
+// 11th June 2024
+// Difficulty - Medium
+// Topic Tags - Arrays, Dynamic Programming, Data Structure, Algorithms & Sorting
+// Asked in the companies like - Amazon & TCS
+
+class Solution {
+    public static long maxTip(int n, int x, int y, int[] arr, int[] brr) {
+        int[][] temp = new int[n][2];
+        for(int i=0;i<n;i++){
+            temp[i][0]=Math.abs(arr[i]-brr[i]);
+            temp[i][1]=i;
+        }
+        Arrays.sort(temp, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] a, int[] b) {
+                if(a[0]>b[0])return 1;
+                else if(a[0]<b[0])return -1;
+                return 0;
+            }
+        });
+        long ans=0;
+        for(int i=n-1;i>=0;i--){
+            int index=temp[i][1];
+            if(arr[index]>brr[index]){
+                if(x!=0){
+                    ans+=arr[index];
+                    x--;
+                }
+                else{
+                    ans+=brr[index];
+                    y--;
+                }
+            }
+            else{
+                if(y!=0){
+                    ans+=brr[index];
+                    y--;
+                }
+                else{
+                    ans+=arr[index];
+                    x--;
+                }
+            }
+        }
+        return ans;
+    }
+}
