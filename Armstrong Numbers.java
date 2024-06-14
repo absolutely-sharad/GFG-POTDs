931fe18a80c357d7cea34a765a71ad8a67eb7f9f
Sharad Singh Kushwaha <153629616+absolutely-sharad@users.noreply.github.com>
2024-06-14 23:42:55 +0530

    Create Armstrong Numbers.java

---
+++ Armstrong Numbers.java
@@ -0,0 +1,17 @@
+// 14th June 2024
+// Difficulty - Easy
+// Topic Tags - Mathematical & Algorithms
+// Asked in the companies like - VMWare & Oracle
+
+class Solution {
+    static String armstrongNumber(int n){
+        // code here
+        int ans=0,temp=n;
+        while(temp != 0){
+            int digit=temp%10;
+            ans+=digit*digit*digit;
+            temp/=10;
+        }
+        return ans==n?"true":"false";
+    }
+}
