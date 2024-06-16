13ad48d838873ad7380ababa6413c825d951f756
Sharad Singh Kushwaha <153629616+absolutely-sharad@users.noreply.github.com>
2024-06-16 16:31:05 +0530

    Create Prime Pair with Target Sum.java

---
+++ Prime Pair with Target Sum.java
@@ -0,0 +1,32 @@
+// 16th June 2024
+// Difficulty - Medium
+// Topic Tags - number-theory, constructive algo, Prime Number, Mathematical
+
+class Solution {
+     public static ArrayList<Integer> getPrimes(int n) {
+        boolean prime[] = new boolean[n+1];
+        Arrays.fill(prime,true);
+        prime[0]=false;
+        prime[1]=false;
+        ArrayList<Integer> ans = new ArrayList<Integer>();
+        for (int i=2;i*i<=n;i++) {
+            if(prime[i]) {
+                for(int j=i*i;j<=n;j+=i)prime[j]=false;
+            }
+        }
+        for(int i=2;i<=n;i++){
+            if(prime[i] && prime[n-i]){
+                ans.add(i);
+                ans.add(n-i);
+                return ans;
+            }
+        }
+        ans.add(-1);
+        ans.add(-1);
+        return ans;
+    }
+}
+
+
+// Time Complexity:- O(NLog(Log(N)))
+// Space Complexity:- O(N)
